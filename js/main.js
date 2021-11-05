4e5094599669611928984e47b063de66bd08d6ca
ken691388 <ken691388@gmail.com>
2021-11-06 00:21:30 +0900

    fix

--- js/main.js
+++ js/main.js
@@ -132,3 +132,48 @@ $(function(){
     $( "#modal-content2" ).css( {"left": ((w - cw)/2) + "px","top": ((h - ch)/2) + "px"} ) ;
     }
  } ) ;
+
+ /* モーダルウィンドウ */
+$(function(){
+    
+    $("#modal-open3").click(function(){
+        //キーボード操作などにより、オーバーレイが多重起動するのを防止する
+        $( this ).blur() ;	//ボタンからフォーカスを外す
+        if( $( "#modal-overlay3" )[0] ) return false ;		//新しくモーダルウィンドウを起動しない (防止策1)
+        //if($("#modal-overlay")[0]) $("#modal-overlay").remove() ;		//現在のモーダルウィンドウを削除して新しく起動する (防止策2)
+        //オーバーレイを出現させる
+        $( "body" ).append( '<div id="modal-overlay3"></div>' ) ;
+        $( "#modal-overlay3" ).fadeIn( "slow" ) ;
+        //コンテンツをセンタリングする
+        centeringModalSyncer() ;
+        //コンテンツをフェードインする
+        $( "#modal-content3" ).fadeIn( "slow" ) ;
+        //[#modal-overlay]、または[#modal-close]をクリックしたら…
+        
+        $( "#modal-overlay3,#modal-close3" ).unbind().click( function(){
+        //[#modal-content]と[#modal-overlay]をフェードアウトした後に…
+        
+            $( "#modal-content3,#modal-overlay3" ).fadeOut( "slow" , function(){
+                    //[#modal-overlay]を削除する
+                    $('#modal-overlay3').remove() ;
+                
+            } ) ;
+        } ) ;
+    } ) ;
+    //リサイズされたら、センタリングをする関数[centeringModalSyncer()]を実行する
+    $( window ).resize( centeringModalSyncer ) ;
+    //センタリングを実行する関数
+    function centeringModalSyncer() {
+    //画面(ウィンドウ)の幅、高さを取得
+    var w = $( window ).width() ;
+    var h = $( window ).height() ;
+    // コンテンツ(#modal-content)の幅、高さを取得
+    // jQueryのバージョンによっては、引数[{margin:true}]を指定した時、不具合を起こします。
+    var cw = $( "#modal-content3" ).outerWidth( {margin:true} );
+    var ch = $( "#modal-content3" ).outerHeight( {margin:true} );
+    var cw = $( "#modal-content3" ).outerWidth();
+    var ch = $( "#modal-content3" ).outerHeight();
+    //センタリングを実行する
+    $( "#modal-content3" ).css( {"left": ((w - cw)/2) + "px","top": ((h - ch)/2) + "px"} ) ;
+    }
+ } ) ;
